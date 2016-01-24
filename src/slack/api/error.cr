e47bb766f7ccd9401b21a72c6ea8678a02e871f0
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-01-24 10:34:54 -0300

    Updated to Crystal 0.11.0

--- src/slack/api/error.cr
+++ src/slack/api/error.cr
@@ -1,5 +1,5 @@
 class Slack::API::Error < Exception
-  def initialize(response : HTTP::Response)
+  def initialize(response : HTTP::Client::Response)
     super("Slack::API::Error: #{response.status_code}\n#{response.body}")
   end
 
