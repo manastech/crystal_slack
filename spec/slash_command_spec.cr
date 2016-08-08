9965ae0980a2ebb7ca89f32d23727fb713679c40
Juan Edi <jedi@manas.com.ar>
2016-08-08 16:58:57 -0300

    Fixed specs

--- spec/slash_command_spec.cr
+++ spec/slash_command_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Slack::SlashCommand do
   it "creates from HTTP::Request" do
     request = HTTP::Request.new "POST", "/",
-      body: "token=some_token&team_id=0&channel_id=1&channel_name=some_channel&user_id=2&user_name=some_user&command=cmd&text=txt"
+      body: "token=some_token&team_id=0&channel_id=1&channel_name=some_channel&user_id=2&user_name=some_user&command=cmd&text=txt&response_url=#{URI.escape("http://slack.com/response")}"
 
     command = Slack::SlashCommand.from_request(request)
     command.token.should eq("some_token")
@@ -14,10 +14,11 @@ describe Slack::SlashCommand do
     command.user_name.should eq("some_user")
     command.command.should eq("cmd")
     command.text.should eq("txt")
+    command.response_url.should eq("http://slack.com/response")
   end
 
   it "creates from request body" do
-    body = "token=some_token&team_id=0&channel_id=1&channel_name=some_channel&user_id=2&user_name=some_user&command=cmd&text=txt"
+    body = "token=some_token&team_id=0&channel_id=1&channel_name=some_channel&user_id=2&user_name=some_user&command=cmd&text=txt&response_url=#{URI.escape("http://slack.com/response")}"
 
     command = Slack::SlashCommand.from_request_body(body)
     command.token.should eq("some_token")
@@ -28,5 +29,6 @@ describe Slack::SlashCommand do
     command.user_name.should eq("some_user")
     command.command.should eq("cmd")
     command.text.should eq("txt")
+    command.response_url.should eq("http://slack.com/response")
   end
 end
