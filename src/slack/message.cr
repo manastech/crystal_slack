a1037b0c504083eb3d59063ad35d16125332d7b3
Mariano Abel Coca <marianoabelcoca@gmail.com>
2020-02-21 14:06:12 -0300

    Release crystal-slack 0.10.0 for Crystal 0.33.0

--- src/slack/message.cr
+++ src/slack/message.cr
@@ -2,13 +2,13 @@ require "json"
 
 class Slack::Message
   JSON.mapping({
-    text:          String,
-    channel:       {type: String, nilable: true},
-    icon_emoji:    {type: String, nilable: true},
-    icon_url:      {type: String, nilable: true},
-    username:      {type: String, nilable: true},
-    attachments:   {type: Array(JSON::Any), nilable: true},
-    response_type: {type: String, nilable: true},
+    text:             String,
+    channel:          {type: String, nilable: true},
+    icon_emoji:       {type: String, nilable: true},
+    icon_url:         {type: String, nilable: true},
+    username:         {type: String, nilable: true},
+    attachments:      {type: Array(JSON::Any), nilable: true},
+    response_type:    {type: String, nilable: true},
     delete_original:  {type: Bool, nilable: true},
     replace_original: {type: Bool, nilable: true},
   })
@@ -21,7 +21,7 @@ class Slack::Message
   end
 
   def send_to_hook(url)
-    HTTP::Client.post(url, form: "payload=#{URI.escape to_json}")
+    HTTP::Client.post(url, form: "payload=#{URI.encode to_json}")
   end
 
   def post_with_api(api)
@@ -29,15 +29,15 @@ class Slack::Message
   end
 
   def add_params(form)
-    form.add "text",           text
-    form.add "channel",        channel             if channel
-    form.add "icon_emoji",     icon_emoji          if icon_emoji
-    form.add "icon_url",       icon_url            if icon_url
-    form.add "username",       username            if username
-    form.add "attachments",    attachments.to_json if attachments
+    form.add "text", text
+    form.add "channel", channel if channel
+    form.add "icon_emoji", icon_emoji if icon_emoji
+    form.add "icon_url", icon_url if icon_url
+    form.add "username", username if username
+    form.add "attachments", attachments.to_json if attachments
 
-    form.add "response_type",  response_type       if response_type
-    form.add "delete_original",  delete_original.to_s         if delete_original
-    form.add "replace_original",  replace_original.to_s       if replace_original
+    form.add "response_type", response_type if response_type
+    form.add "delete_original", delete_original.to_s if delete_original
+    form.add "replace_original", replace_original.to_s if replace_original
   end
 end
